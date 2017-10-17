29378bf622d635a16d60642a7cbd484d704b3be2
isaac jarquin <isaacjarquin@gmail.com>
2017-10-17 23:09:19 +0100

    Add search form screen

--- App.js
+++ App.js
@@ -1,21 +1,15 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text } from 'react-native';
+import {
+  StackNavigator,
+} from 'react-navigation';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
-}
+import Home from './screens/home'
+import searchForm from './screens/search-form'
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const App = StackNavigator({
+  Home: { screen: Home },
+  SearchForm: { screen: searchForm },
 });
+
+module.exports = App
---
+++ screens/home.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image, TouchableHighlight, Navigator } from 'react-native';
+
+export default class Home extends React.Component {
+  constructor (props) {
+    super(props)
+    this.onAddStarted = this.onAddStarted.bind(this)
+  }
+
+  onAddStarted() {
+      const { navigate } = this.props.navigation;
+
+      navigate('SearchForm', { name: 'Jane' })
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+          <Image
+                source={require('../assets/images/mascotas_y_personas.jpg')}
+                style={{width: 600, height: 300}}
+                resizeMode= { Image.resizeMode.contain }
+          />
+          <TouchableHighlight
+              style={styles.button}
+              onPress={this.onAddStarted}>
+                <Text style={styles.buttonText} >
+                  Buscar Mascota perdida
+                </Text>
+          </TouchableHighlight>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    height: 60,
+    borderColor: '#05A5D1',
+    borderWidth: 2,
+    backgroundColor: '#333',
+    margin: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#FAFAFA',
+    fontSize: 20,
+    fontWeight: '600',
+  }
+});
