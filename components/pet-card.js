c920f4d8f664762be69dc4c42f4b03c4f112d038
isaac jarquin <isaacjarquin@gmail.com>
2017-10-21 00:45:24 +0100

    add more info to pet card

--- components/pet-card.js
+++ components/pet-card.js
@@ -4,11 +4,15 @@ import { Card, Button } from 'react-native-elements'
 
 export default class PetCard extends React.Component {
   render() {
-    const { kind, info, image } = this.props
+    const { kind, info, image, location, date, breed } = this.props
+    const cardTitleWithBreed = `${kind}, de raza ${breed}`
+    const cardTitle = breed ? cardTitleWithBreed : kind
+
     return (
       <View style={styles.container}>
-        <Card title={kind} image={{uri: image}}>
-          <Text style={{marginBottom: 10}}>{info}</Text>
+        <Card title={cardTitle} image={{uri: image}}>
+          <Text style={styles.secondaryTitle}>Encontrado en {location}, el {date}</Text>
+          <Text style={styles.description}>{info}</Text>
           <Button buttonStyle={styles.button} title='Contactar' />
         </Card>
       </View>
@@ -28,5 +32,12 @@ const styles = StyleSheet.create({
     marginLeft: 0,
     marginRight: 0,
     marginBottom: 0
+  },
+  secondaryTitle: {
+    marginBottom: 10,
+    fontWeight: 'bold',
+  },
+  description: {
+    marginBottom: 20
   }
 });
