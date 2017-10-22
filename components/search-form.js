59de350b38f4fe934b5fc07a125c12d24d16a301
isaac jarquin <isaacjarquin@gmail.com>
2017-10-22 11:31:36 +0100

    add selector to filter in search form

--- components/search-form.js
+++ components/search-form.js
@@ -1,23 +1,44 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import bodyParamsBuilder from '../utils/http-request.js'
-import { FormLabel, FormInput } from 'react-native-elements'
+import locations from '../data/locations'
+import pets from '../data/pets'
+import { FormLabel, FormInput, Divider } from 'react-native-elements'
 import { Button } from 'react-native-elements'
-
+import {Select, Option} from "react-native-chooser";
 
 export default class SearchForm extends React.Component {
   constructor (props, context) {
     super(props, context)
     this.getPets = this.getPets.bind(this)
+    this.setType = this.setType.bind(this)
+    this.setProvince = this.setProvince.bind(this)
+    this.setAutonomousComunity = this.setAutonomousComunity.bind(this)
 
     this.state = {
       type: '',
       autonomousComunity: '',
-      province: ''
+      province: '',
+      provincias: []
     }
   }
 
-  getPets(event) {
+  setType(text) {
+    this.setState({type: text})
+  }
+
+  setProvince(text) {
+    this.setState({province: text})
+  }
+
+  setAutonomousComunity(text) {
+    const location = locations.find((location) => location.value === text)
+
+    this.setState({autonomousComunity: text})
+    this.setState({provincias: location.provincias})
+  }
+
+  getPets() {
     const url = `https://items-api.herokuapp.com/api/items${bodyParamsBuilder(this.state)}`
 
     return fetch(url)
@@ -36,16 +57,46 @@ export default class SearchForm extends React.Component {
       <View style={styles.container}>
         <View>
           <FormLabel>Tipo de mascota</FormLabel>
-          <FormInput onChangeText={(text) => this.setState({type: text})}/>
+          <Select
+              defaultText={"perro"}
+              style={styles.select}
+              textStyle={{color: "grey"}}
+              backdropStyle={{backgroundColor : "#d3d5d6"}}
+              optionListStyle={{backgroundColor : "#F5FCFF"}}
+              onSelect={this.setType}
+              selected={() => setSelectedText(this.state.type)}
+            >
+            {pets.map((pet) => <Option key={pet.id} value={pet.value}>{pet.value}</Option> )}
+          </Select>
 
-          <FormLabel>Comunidad autónoma</FormLabel>
-          <FormInput onChangeText={(text) => this.setState({autonomousComunity: text})}/>
+          <FormLabel>Comunidad autonoma</FormLabel>
+          <Select
+              defaultText={"Seleccione la comunidad autonoma"}
+              style={styles.select}
+              textStyle={{color: "grey"}}
+              backdropStyle={{backgroundColor : "#d3d5d6"}}
+              optionListStyle={{backgroundColor : "#F5FCFF"}}
+              onSelect={this.setAutonomousComunity}
+              selected={() => setSelectedText(this.state.autonomousComunity)}
+            >
+            {locations.map((location) => <Option key={location.id} value={location.value}>{location.value}</Option> )}
+          </Select>
 
           <FormLabel>Provincia</FormLabel>
-          <FormInput onChangeText={(text) => this.setState({province: text})}/>
-        </View>
+            <Select
+                defaultText={"Seleccione la provincia"}
+                style={styles.select}
+                textStyle={{color: "grey"}}
+                backdropStyle={{backgroundColor : "#d3d5d6"}}
+                optionListStyle={{backgroundColor : "#F5FCFF"}}
+                onSelect={this.setProvince}
+                selected={() => setSelectedText(this.state.province)}
+              >
+              {this.state.provincias.map((provincia) => <Option key={provincia.id} value={provincia.value}>{provincia.value}</Option> )}
+            </Select>
 
-        <View style={styles.innerContainer}>
+          <Divider style={styles.divider} />
+
           <Button
             style={styles.button}
             large
@@ -65,15 +116,20 @@ const styles = StyleSheet.create({
     backgroundColor: '#F7F7F7',
     paddingTop: 20,
   },
-  innerContainer: {
-    flex: 1,
-    backgroundColor: '#fff',
-    justifyContent: 'flex-end',
-    backgroundColor: '#F7F7F7',
-    paddingTop: 20,
+  divider: {
+    marginRight: 20,
+    marginLeft: 20,
+    marginBottom: 20,
+    backgroundColor: '#d3d5d6'
   },
   button: {
     marginBottom: 20
+  },
+  select: {
+    alignSelf: 'stretch',
+    margin: 20,
+    borderWidth : 1,
+    borderColor : "#d3d5d6"
   },
   buttonText: {
     color: '#FAFAFA',
