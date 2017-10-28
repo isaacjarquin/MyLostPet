e75ee92c4b4f94bbf7fa9990b54dc54e4cf0a231
isaac jarquin <isaacjarquin@gmail.com>
2017-10-28 12:37:54 +0100

    add test for isInvalidForm

--- tests/unit/utils/validations.test.js
+++ tests/unit/utils/validations.test.js
@@ -3,7 +3,8 @@ import {
   presence,
   isValidNumber,
   isValidEmail,
-  setValidation
+  setValidation,
+  isInvalidForm
 } from "../../../utils/validations"
 
 describe("validations", () => {
@@ -59,6 +60,76 @@ describe("validations", () => {
         validationMessage: "validation test",
         validationMessageColor: "red",
         validationFieldBorderColor: "red"
+      })
+    })
+  })
+
+  describe("isValiForm", () => {
+    describe("when is a valid form", () => {
+      const name = {value: 'test name'}
+      const email = {value: 'test@gmail.com'}
+      const phoneNumber = {value: 987465364}
+
+      const fields = [
+        { field: name, validate: presence},
+        { field: email, validate: isValidEmail},
+        { field: phoneNumber, validate: isValidNumber}
+      ]
+
+      test("returns false when all the fields are correct", () => {
+        expect(isInvalidForm(fields)).toBe(false)
+      })
+    })
+
+    describe("when is an invalid form", () => {
+      test("return true if name is not present", () => {
+        const fields = [
+          { field: {value: ''}, validate: presence},
+          { field: {value: 'test@gmail.com'}, validate: isValidEmail},
+          { field: {value: 987465364}, validate: isValidNumber}
+        ]
+
+        expect(isInvalidForm(fields)).toBe(true)
+      })
+
+      test("return true if email is not present", () => {
+        const fields = [
+          { field: {value: 'some value'}, validate: presence},
+          { field: {value: ''}, validate: isValidEmail},
+          { field: {value: 987465364}, validate: isValidNumber}
+        ]
+
+        expect(isInvalidForm(fields)).toBe(true)
+      })
+
+      test("return true if email has an invalid format", () => {
+        const fields = [
+          { field: {value: 'some value'}, validate: presence},
+          { field: {value: 'test@test'}, validate: isValidEmail},
+          { field: {value: 987465364}, validate: isValidNumber}
+        ]
+
+        expect(isInvalidForm(fields)).toBe(true)
+      })
+
+      test("return true if phoneNumber is not present", () => {
+        const fields = [
+          { field: {value: 'some value'}, validate: presence},
+          { field: {value: 'test@test.com'}, validate: isValidEmail},
+          { field: {value: ''}, validate: isValidNumber}
+        ]
+
+        expect(isInvalidForm(fields)).toBe(true)
+      })
+
+      test("return true if phoneNumber is an invalid number", () => {
+        const fields = [
+          { field: {value: 'some value'}, validate: presence},
+          { field: {value: 'test@test.com'}, validate: isValidEmail},
+          { field: {value: "2345c4323"}, validate: isValidNumber}
+        ]
+
+        expect(isInvalidForm(fields)).toBe(true)
       })
     })
   })
--- utils/validations.js
+++ utils/validations.js
@@ -4,11 +4,13 @@ const presence = ({value}) => {
 
 const isValidNumber = ({value}) => {
     let reg = new RegExp(/^\d+(?:\.\d{1,2})?$/)
+
     return reg.test(value)
 }
 
 const isValidEmail = ({value}) => {
     let reg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
     return reg.test(value)
 }
 
@@ -21,6 +23,7 @@ const setValidation = (message) => {
 }
 
 const isInvalidForm = (fields) => {
+
   return fields.map(({field, validate}) => validate(field)).includes(false)
 }
 
