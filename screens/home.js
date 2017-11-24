df53ad42ab3ffc4b8fb34a2fc54ed263f528046f
isaac jarquin <isaacjarquin@gmail.com>
2017-11-24 22:13:32 +0000

    add share button

--- screens/home.js
+++ screens/home.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { StyleSheet, Text, View, Image } from "react-native"
+import { StyleSheet, Text, View, Image, TouchableOpacity, Share } from "react-native"
 import { Button } from "react-native-elements"
 import SocialIconsModal from "../components/social-icons-modal"
 
@@ -8,6 +8,7 @@ export default class Home extends React.Component {
 		super(props)
 		this.navigateToSearchForm = this.navigateToSearchForm.bind(this)
 		this.navigateToMissingPetForm = this.navigateToMissingPetForm.bind(this)
+		this._shareMyLostPetWeb = this._shareMyLostPetWeb.bind(this)
 
 		this.state = {
 	    isModalVisible: false
@@ -24,6 +25,16 @@ export default class Home extends React.Component {
 
 		navigate("SearchForm")
 	}
+
+	_shareMyLostPetWeb () {
+    Share.share({
+      message: 'http://www.mylostpet.es/',
+      title: 'My Lost Pet',
+      url: 'http://www.mylostpet.es/'
+    })
+    .then(this._showResult)
+    .catch(err => console.log(err))
+  }
 
 	render () {
 		return (
@@ -55,7 +66,9 @@ export default class Home extends React.Component {
 						onPress={this.navigateToSearchForm}
 						title='Buscar Mascota perdida' />
 
-					<SocialIconsModal />
+					<TouchableOpacity style={styles.share} onPress={this._shareMyLostPetWeb} >
+						<Text style={styles.shareText} >Share</Text>
+					</TouchableOpacity>
 				</Image>
 			</View>
 		)
@@ -93,6 +106,21 @@ const styles = StyleSheet.create({
 		height: 100,
 		marginBottom: 190,
 		alignSelf: "center"
+	},
+	share: {
+		backgroundColor: "#333333",
+		opacity: 0.8,
+		marginLeft: 15,
+		marginRight: 15,
+		marginTop: 5,
+		borderTopLeftRadius: 3,
+		borderTopRightRadius: 3,
+		padding: 8
+	},
+	shareText: {
+		alignSelf: "center",
+		color: "white",
+		fontSize: 18
 	},
 	image: {
 		flex: 1,
