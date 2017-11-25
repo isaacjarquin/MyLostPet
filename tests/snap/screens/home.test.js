7855f73a2a0eba83e40f0f6ca2583be8103c2cf6
isaac jarquin <isaacjarquin@gmail.com>
2017-11-25 21:39:19 +0000

    fix broken test

--- tests/snap/screens/home.test.js
+++ tests/snap/screens/home.test.js
@@ -5,8 +5,10 @@ import Home from "../../../screens/home"
 import renderer from "react-test-renderer"
 
 it("renders correctly", () => {
+	const navigation = { navigate: "some navigation function" }
+
 	const tree = renderer.create(
-		<Home />
+		<Home navigation={navigation} />
 	).toJSON()
 	expect(tree).toMatchSnapshot()
 })
