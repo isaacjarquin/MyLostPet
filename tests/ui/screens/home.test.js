19be9178335cba089c02abbe037ccfa5f660fa01
isaac jarquin <isaacjarquin@gmail.com>
2017-12-27 12:31:23 +0000

    add more coverage to home test

--- tests/ui/screens/home.test.js
+++ tests/ui/screens/home.test.js
@@ -1,14 +1,65 @@
 import "react-native"
 import React from "react"
 import Home from "../../../screens/home"
+import { shallow } from 'enzyme'
+import toJSON from 'enzyme-to-json'
 
 import renderer from "react-test-renderer"
 
-it("renders correctly", () => {
-    const navigation = { navigate: "some navigation function" }
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
-    const tree = renderer.create(
-        <Home navigation={navigation} />
-    ).toJSON()
-    expect(tree).toMatchSnapshot()
+configure({ adapter: new Adapter() });
+
+describe("Home", () => {
+    const navigate = jest.fn()
+
+    const props = {
+        navigation: {
+            navigate: navigate
+        }
+    };
+
+    beforeEach(() => {
+        wrapper = shallow(<Home {...props} />);
+    });
+
+    afterEach(() => {
+        navigate.mockReset()
+    });
+
+    it("renders correctly", () => {
+        expect(wrapper).toBeDefined()
+        expect(toJSON(wrapper)).toMatchSnapshot();
+    })
+
+    describe("navigateToMissingPetForm", () => {
+        beforeEach(() => {
+            wrapper.instance().navigateToMissingPetForm()
+        })
+
+        it("navigate to MissingPetForm", () => {
+            expect(navigate).toHaveBeenCalledTimes(1);
+            expect(navigate).toHaveBeenCalledWith("MissingPetForm");
+        })
+    })
+
+    describe("navigateToSearchForm", () => {
+        beforeEach(() => {
+            wrapper.instance().navigateToSearchForm()
+        })
+
+        it("navigate to MissingPetForm", () => {
+            expect(navigate).toHaveBeenCalledTimes(1);
+            expect(navigate).toHaveBeenCalledWith("SearchForm");
+        })
+    })
+
+    describe("_showModal", () => {
+        it("modify isModalVisible state when called", () => {
+            wrapper.setState({ isModalVisible: false })
+            wrapper.instance()._showModal()
+            expect(wrapper.state().isModalVisible).toBe(true);
+        })
+    })
 })
